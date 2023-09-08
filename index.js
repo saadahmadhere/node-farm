11b6e53ebc151855957d15e21f75b90dc40084f9
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2023-09-08 17:29:43 +0530

    add a server that listens on port 8000

--- index.js
+++ index.js
@@ -1,27 +1,12 @@
 const fs = require('fs');
+const http = require('http');
 
-// Blocking and synchronous way...
-// const textIp = fs.readFileSync('./txt/input.txt', 'utf-8');
-
-// const textOut = `about avocadoes: ${textIp}\n created on ${Date.now()}`;
-
-// fs.writeFileSync('./txt/output.txt', textOut);
-
-// console.log('file written successfully.');
+const server = http.createServer((req, res) => {
+  console.log(req)
+  res.end('Hello from the server');
+});
 
 
-// Non-blocking and asynchronous way...
-
-fs.readFile('./txt/start.txt','utf-8', (err, data) => {
-  console.log('read file 1 💥')
-  fs.readFile(`./txt/${data}.txt`,'utf-8', (err, data1) => {
-    console.log('🤍reading from read-this: ', data1)
-    fs.readFile(`./txt/append.txt`,'utf-8', (err, data2) => {
-      fs.writeFile('./txt/final.txt', `${data1}\n${data2}`, 'utf-8', err => {
-        console.log('file written successfully.')
-      })
-    })
-  })
+server.listen(8000, '127.0.0.1', () => {
+  console.log('💥listening to request on port 8000')
 })
-
-console.log('💥you will see this before the readFile is executed...')
