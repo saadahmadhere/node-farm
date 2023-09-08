69ba20a89fdcdd8b6f8d197b6b6037d1e3d65015
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2023-09-08 17:43:03 +0530

    add routes and handle page not found case

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ const fs = require('fs');
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(req)
-  res.end('Hello from the server');
+  const {url: pathName} = req;
+
+  if(pathName === '/overview' || pathName === 'overview'){
+    res.end("this is the overview")
+  }else if(pathName === '/product'){
+    res.end("this is the product")
+  }else{
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world'
+    })
+    res.end("<h2 style={color: 'blue'}>page not found</h2>")
+  }
+  // res.end('Hello from the server');
 });
 
 
