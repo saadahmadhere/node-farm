a7390890d26d5be88f884ae1575181ffb4d6b1d9
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2023-09-13 16:14:37 +0530

    use slugify

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const slugify = require('slugify');
 
 const replaceTemplate = require('./modules/replaceTemplate');
 
@@ -10,18 +11,18 @@ const tempOverview = fs.readFileSync(
 	`${__dirname}/templates/template-overview.html`,
 	'utf-8'
 );
-
 const tempCard = fs.readFileSync(
 	`${__dirname}/templates/template-card.html`,
 	'utf-8'
 );
-
 const tempProduct = fs.readFileSync(
 	`${__dirname}/templates/template-product.html`,
 	'utf-8'
 );
 
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map(el => slugify(el.productName, {lower: true}));
 
 const server = http.createServer((req, res) => {
 	const { pathname, query } = url.parse(req.url, true);
