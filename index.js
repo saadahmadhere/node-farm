bb6a73a792f63d06cff9314c4437c1aad727617c
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2023-09-13 15:32:12 +0530

    making things work

--- index.js
+++ index.js
@@ -1,52 +1,46 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
+
+function replaceTemplate(temp, product) {
+  // temp should be a string and product should be an object.
+	let output = temp.replace(/{%productName%}/g, product.productName);
+	output = output.replace(/{%image%}/g, product.image);
+	output = output.replace(/{%price%}/g, product.price);
+	output = output.replace(/{%from%}/g, product.from);
+	output = output.replace(/{%nutrients%}/g, product.nutrients);
+	output = output.replace(/{%quantity%}/g, product.quantity);
+	output = output.replace(/{%description%}/g, product.productName);
+	output = output.replace(/{%id%}/g, product.id);
+
+	if (!product.organic)
+		output = output.replace(/{%not-organic%}/g, 'not-organic');
+	return output;
+}
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+
+const tempOverview = fs.readFileSync(
+	`${__dirname}/templates/template-overview.html`,
+	'utf-8'
+);
+
+const tempCard = fs.readFileSync(
+	`${__dirname}/templates/template-card.html`,
+	'utf-8'
+);
+
+const tempProduct = fs.readFileSync(
+	`${__dirname}/templates/template-product.html`,
+	'utf-8'
+);
+
+const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-	const { url: pathName } = req;
-	const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
-	const tempOverview = fs.readFileSync(
-		`${__dirname}/templates/template-overview.html`,
-		'utf-8'
-	);
-	const tempCard = fs.readFileSync(
-		`${__dirname}/templates/template-card.html`,
-		'utf-8'
-	);
-	const tempProduct = fs.readFileSync(
-		`${__dirname}/templates/template-product.html`,
-		'utf-8'
-	);
-	const dataObj = JSON.parse(data);
+	const { pathname, query } = url.parse(req.url, true);
 
-	function replaceTemplate(temp, product) {
-    // console.log(product.productName)
-    let output = temp.replace(/{%productName%}/g, product.productName)
-    output = output.replace(/{%image%}/g, product.image)
-    output = output.replace(/{%price%}/g, product.price)
-    output = output.replace(/{%from%}/g, product.from)
-    output = output.replace(/{%nutrients%}/g, product.nutrients)
-    output = output.replace(/{%quantity%}/g, product.quantity)
-    output = output.replace(/{%description%}/g, product.productName)
-    output = output.replace(/{%id%}/g, product.id)
-
-    if(!product.organic) output = output.replace(/{%not-organic%}/g, 'not-organic');
-    return output;
-
-		// let temp = card;
-		// Object.entries(el).forEach(([key, val], i) => {
-		// 	if (key !== 'id') {
-		// 		if (i == 1) {
-		// 			console.log(`{%${key%}}`);
-    //       console.log(val)
-		// 		}
-		// 		temp.replaceAll(`{%${key%}}`, val);
-		// 	}
-		// });
-
-		// return temp;
-	}
-
-	if (pathName === '/overview' || pathName === '/') {
+  if (pathname === '/overview' || pathname === '/') {
 		res.writeHead(200, {
 			'Content-type': 'text/html',
 		});
@@ -54,12 +48,17 @@ const server = http.createServer((req, res) => {
 		const cardsHtml = dataObj
 			.map((el) => replaceTemplate(tempCard, el))
 			.join('');
+
 		const output = tempOverview.replace('{%productCards%}', cardsHtml);
 		res.end(output);
-		// res.end(tempOverview);
-	} else if (pathName === '/product') {
-		res.end('this is the product');
-	} else if (pathName === '/api') {
+	} else if (pathname === '/product') {
+    const product = dataObj[query?.id];
+
+		const output = replaceTemplate(tempProduct, product);
+
+		res.end(output);
+
+	} else if (pathname === '/api') {
 		res.writeHead(200, { 'content-type': 'application/json' });
 		res.end(data);
 	} else {
@@ -67,9 +66,8 @@ const server = http.createServer((req, res) => {
 			'Content-type': 'text/html',
 			'my-own-header': 'hello-world',
 		});
-		res.end("<h2 style={color: 'blue'}>page not found</h2>");
+		res.end('<h2>page not found</h2>');
 	}
-	// res.end('Hello from the server');
 });
 
 server.listen(8000, '127.0.0.1', () => {
